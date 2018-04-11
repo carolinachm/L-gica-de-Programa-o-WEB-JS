0d53658d0e40faf3f1107055cbe1273ab912f29c
carolinachm <carolti2013@gmail.com>
2018-04-11 13:45:22 -0300

    finalização do cadastro

---
+++ Modulo1/desafio1/js/tela.js
@@ -0,0 +1,12 @@
+"user strict";
+
+function somarValores(){
+    var s1 = document.getElementById("numero1").value;
+    var s2 = document.getElementById("numero2").value;
+        var s3 = s1*s2;
+        alert(s3);
+}
+function limpar(){
+    document.getElementById("numero1").value;
+    document.getElementById("numero2").value;
+}
---
+++ Modulo1/exercicio/exercicio2/js/exercicio2.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function abrirMenu(){
+    document.getElementById("menu").classList.add("menu_aberto")
+}
+function fecharMenu() {
+    document.getElementById("menu").classList.remove("menu_aberto")
+}
