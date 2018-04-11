2ea50ea0a43351441c964bb9ce9793ad3cd03d0a
carolinachm <carolti2013@gmail.com>
2018-04-11 17:58:15 -0300

    correção do calculo de porcentagem

--- Modulo1/desafio1/js/tela.js
+++ Modulo1/desafio1/js/tela.js
@@ -1,52 +1,54 @@
 "user strict";
 
-class Calcularoda{
+class Calcularoda {
 
-    botao(valor){
+    botao(valor) {
         document.calc.visor.value += valor;
     }
 
-     somarValor(){
+    somarValor() {
         //1) ler dados convertido em numeros dos inputs
         var s1 = parseFloat(document.getElementById("s1").value);
         var s2 = parseFloat(document.getElementById("s2").value);
         //realizar a soma
-        var resultado = s1+s2;
+        var resultado = s1 + s2;
         document.getElementById('resultado').value = resultado;
     }
-     subtrairValor(){
+    subtrairValor() {
         var s1 = parseFloat(document.getElementById("s1").value);
         var s2 = parseFloat(document.getElementById("s2").value);
-        var resultado = s1-s2;
+        var resultado = s1 - s2;
         document.getElementById('resultado').value = resultado;
     }
-     multiplicarValor(){
+    multiplicarValor() {
         var s1 = parseFloat(document.getElementById("s1").value);
         var s2 = parseFloat(document.getElementById("s2").value);
         var resultado = s1 * s2;
         document.getElementById('resultado').value = resultado;
     }
-     dividirValor(){
+    dividirValor() {
         var s1 = parseFloat(document.getElementById("s1").value);
         var s2 = parseFloat(document.getElementById("s2").value);
-        var resultado = s1 / s2;
-        document.getElementById('resultado').value = resultado;
+        var result, total;
+        result = valor / 100;
+        total = valor - (porcentagem * result);
+        document.getElementById('resultado').value = total;
     }
-     porcentagem(){
+    porcentagem() {
         //1) ler dados convertido em numeros dos inputs
         var s1 = parseFloat(document.getElementById("s1").value);
         var s2 = parseFloat(document.getElementById("s2").value);
         //realizar a soma
-        var resultado = parseFloat(s1*(s2/100));
+        var resultado = parseFloat(s1 * (s2 / 100));
         document.getElementById('resultado').value = resultado;
     }
-     limpar(){
-         //limpar os campos
-         document.calc.visor.value = '';
+    limpar() {
+        //limpar os campos
+        document.calc.visor.value = '';
     }
-    calcular(){
+    calcular() {
         var resultado = 0;
-        resultado =document.calc.visor.value;
+        resultado = document.calc.visor.value;
         document.calc.visor.value = '';
         document.calc.visor.value = eval(resultado);
     }
