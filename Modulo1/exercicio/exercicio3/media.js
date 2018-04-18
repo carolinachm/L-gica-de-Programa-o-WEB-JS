6a28d546bf3803aad22f5880ccf0693438ee989f
carolinachm <carolti2013@gmail.com>
2018-04-18 17:42:03 -0300

    exercicio calculo da media

---
+++ Modulo1/exercicio/exercicio3/media.js
@@ -0,0 +1,17 @@
+"user strict";
+
+class Media{
+    calcular(){
+        //ler variaveis
+        var n1 = parseFloat(document.getElementById("n1").value);
+        var n2 =parseFloat(document.getElementById("n2").value);
+
+        //calcular media
+        var media =parseFloat((n1 + n2)/2);
+
+        document.getElementById('media').value;
+       
+    }
+}
+//instancia do objeto
+var media = new Media();
